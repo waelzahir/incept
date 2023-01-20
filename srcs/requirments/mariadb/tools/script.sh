a5487008e709d6b2fa88254dc031b9efa91b9d67
weazah <minimala978@gmail.com>
2023-01-20 05:41:54 +0100

    mariadb prolly done

--- srcs/requirments/mariadb/tools/script.sh
+++ srcs/requirments/mariadb/tools/script.sh
@@ -1,5 +1,14 @@
 #! /bin/bash
-usermod -a -G mysql mysql
-mkdir -p /var/run/mysqld
-chown -R mysql:mysql /var/run/mysqld/
+usermod -a -G mysql mysql 
+mkdir -p /var/run/mysqld 
+chown -R mysql:mysql /var/run/mysqld/ 
+
+if test -f "/var/run/mysqld/hiddenf.sql"; then
+    echo "database already configured"
+else
+    envsubst < /root/mysql.conf > /var/run/mysqld/hiddenf.sql
+    service  mysql start
+    mysql -u root < /var/run/mysqld/hiddenf.sql
+    service mysql stop
+fi
 mysqld
---
+++ srcs/sc.sh
@@ -0,0 +1,5 @@
+if test -d "rdfequirments"; then
+    echo "db exist"
+else   
+	echo "doesnt"
+fi
