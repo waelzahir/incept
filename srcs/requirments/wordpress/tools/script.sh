39edfcf4eacf885ce3c4a5c3dcb19da849676f35
waelzahir <ozahir@student.1337.ma>
2023-05-17 01:37:02 +0100

    progress save

--- srcs/requirments/wordpress/tools/script.sh
+++ srcs/requirments/wordpress/tools/script.sh
@@ -1,6 +1,13 @@
 echo wordpress
 ifconfig | grep inet
 echo wordpress
-mkdir -p /run/php
-touch /run/php/php7.3-fpm.sock
+if [ -d "/var/www/html/blog" ] 
+then
+    echo  -n
+else
+    mkdir -p /var/www/html/blog
+    mv /root/wpfiles/* /var/www/html/blog
+    wp --allow-root core config --path='/var/www/html/blog' --dbhost=test --dbname=test --dbuser=test --dbpass=test
+    wp --allow-root core install  --path='/var/www/html/blog' --url='ozahir.42.fr' --title='42 blog' --admin_name="admin" --admin_password='admin' --admin_email='ozahir@student.1337.ma'
+fi
 exec "$@"
