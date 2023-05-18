b486ca6cf0dcdf427f5aaeea40c6df3f1f599ce5
waelzahir <ozahir@student.1337.ma>
2023-05-18 04:42:40 +0100

    mariadb close to finish

--- srcs/requirments/mariadb/tools/script.sh
+++ srcs/requirments/mariadb/tools/script.sh
@@ -1,4 +1,15 @@
 echo maria
 ifconfig | grep inet
 echo maria
+if [ -d "/var/lib/mysql/wordpress" ] 
+then
+    echo  -n
+else
+   service mysql start
+   mysql_secure_installation   < /root/mysql_default > /dev/null 2>&1
+   mysql -u root < /root/database_conf > /dev/null 2>&1
+#    mysqladmin -u root shutdown
+#    kill $(cat /run/mysqld/mysqld.pid)
+fi
+sleep 20
 exec "$@"
