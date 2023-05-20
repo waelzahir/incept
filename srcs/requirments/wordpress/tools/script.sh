1d4b5df852b4095d40dc70811eee7cfb7713ed76
Ouail Zahir <ozahir@e2r12p4.1337.ma>
2023-05-20 01:24:12 +0100

    yes yes

--- srcs/requirments/wordpress/tools/script.sh
+++ srcs/requirments/wordpress/tools/script.sh
@@ -1,14 +1,14 @@
 echo wordpress
 ifconfig | grep inet
 echo wordpress
-if [ -d "/var/www/html/blog" ] 
+cp /root/www.conf /etc/php/7.3/fpm/pool.d/
+if [ -f "/var/www/html/wp-config.php" ] 
 then
     echo  -n
 else
     sleep 20
-    mkdir -p /var/www/html/blog
-    mv /root/wpfiles/* /var/www/html/blog
-    wp --allow-root config create --path='/var/www/html/blog' --dbhost="${MARIATARGET}" --dbname="${DATABASE}" --dbuser="${MYSQL_USER}" --dbpass="${MYSQL_USER_PASSWORD}"
-    wp --allow-root core install  --path='/var/www/html/blog' --url='localhost' --title='42 blog' --admin_name="${WORDPRESS_ADMIN}" --admin_password="${WORDPRESS_ADMIN_PASSWORD}" --admin_email="${WORDPRESS_ADMIN}@student.1337.ma"
+    mv /root/wpfiles/* /var/www/html/
+    wp --allow-root config create --path='/var/www/html/' --dbhost="${MARIATARGET}" --dbname="${DATABASE}" --dbuser="${MYSQL_USER}" --dbpass="${MYSQL_USER_PASSWORD}"
+    wp --allow-root core install  --path='/var/www/html/' --url='localhost' --title='42 blog' --admin_name="${WORDPRESS_ADMIN}" --admin_password="${WORDPRESS_ADMIN_PASSWORD}" --admin_email="${WORDPRESS_ADMIN}@student.1337.ma"
 fi
 exec "$@"
